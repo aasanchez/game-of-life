2c08b3d3290b006b7bbf4842ce921314c588205b
Alexis Sánchez <aasanchez@gmail.com>
2020-10-19 22:21:41 +0200

    Working

--- src/src/Command/LifeCommand.php
+++ src/src/Command/LifeCommand.php
@@ -7,23 +7,30 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-class LifeCommand extends Command {
+class LifeCommand extends Command
+{
 
     protected static $defaultName = 'life:new-game';
+
+    /** @var array */
     public $cells = [];
+
+    /** @var array */
     public $opt = [];
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->opt = [
-            "height" => exec('tput lines') - 7,
-            "width" => exec('tput cols'),
-            "FPS"=>60,
-            "odds"=>5
+            "height" => intval(exec('tput lines')) - 7,
+            "width" => intval(exec('tput cols')),
+            "FPS" => 10,
+            "odds" => 1,
         ];
         parent::__construct();
     }
 
-    protected function configure() {
+    protected function configure(): void
+    {
         $this
             ->setDescription('Start a new Game')
             ->setHelp('
@@ -52,53 +59,122 @@ condensed into the following:
 ');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output) {
-        $frame = 0;
-        $time = 0;
-        $io = new SymfonyStyle($input, $output);
-        $game = $this->generateCells();
+    protected function execute(InputInterface $input, OutputInterface $output) : int
+    {
+        $status = new SymfonyStyle($input, $output);
+        $startTime = microtime(true);
+        $this->generateCells();
+
         while (true) {
-            $frame++;
             $output->write($this->ClearScreen());
-            foreach ($this->cells as $key => $row) {
-                $print_row = null;
-                foreach ($row as $key2 => $val) {
-                    $char = " ";
-                    if ($this->cells[$key][$key2] == 1) {
-                        $char = "#";
+            foreach ($this->cells as $y => $row) {
+                $toPrint = '';
+                foreach (array_keys($row) as $x) {
+                    $state = " ";
+                    if ($this->cells[$y][$x] == 1) {
+                        $state = "█";
                     }
-                    $print_row .= $char;
+                    $toPrint .= $state;
                 }
-                $output->writeln($print_row);
+                $output->writeln($toPrint);
             }
 
+            $endTime = microtime(true);
 
-            if ($frame == $this->opt["FPS"]) {
-                $time++;
-                $frame = 0;
-            }
-            $io->table(
-                ['X', 'Y', 'Seconds'],
+            $status->table(
+                ['Width', 'Height', 'Time', 'Living cells', 'FPS'],
                 [
-                    [$this->opt["width"], $this->opt["height"], $time." sec"],
+                    [
+                        $this->opt["width"],
+                        $this->opt["height"],
+                        $this->getTime($endTime - $startTime),
+                        $this->countCells().'/'.$this->opt["width"] * $this->opt["height"],
+                        $this->opt["FPS"],
+                    ],
                 ]
             );
+
+            //New Generation
+            $this->newGeneration();
+
             usleep(1000000 / $this->opt["FPS"]);
         }
-//        return Command::SUCCESS;
+        return Command::SUCCESS;
     }
 
-    protected function ClearScreen() {
+    protected function newGeneration(): void
+    {
+        $grid = $this->cells;
+
+        [$toKill, $toBorn] = $this->neighborsWeeper($grid);
+
+        foreach ($toKill as $c) {
+            $grid[$c[0]][$c[1]] = 0;
+        }
+
+        foreach ($toBorn as $c) {
+            $grid[$c[0]][$c[1]] = 1;
+        }
+
+        $this->cells = $grid;
+    }
+
+    protected function neighborsWeeper(array $grid) : array
+    {
+        $toKill = [];
+        $toBorn = [];
+        for ($y = 0; $y < $this->opt["height"]; $y++) {
+            for ($x = 0; $x < $this->opt["width"]; $x++) {
+                $neighbors = $this->getNeighbors($x, $y);
+
+                if ($grid[$y][$x] && ($neighbors < 2 || $neighbors > 3)) {
+                    $toKill[] = [$y, $x];
+                }
+                if (!$grid[$y][$x] && $neighbors === 3) {
+                    $toBorn[] = [$y, $x];
+                }
+            }
+        }
+
+        return [$toKill, $toBorn];
+    }
+
+    /**
+     *
+     * @SuppressWarnings("CyclomaticComplexity")
+     */
+    protected function getNeighbors(int $xAxis, int $yAxis) : int
+    {
+        $neighbors = 0;
+        for ($y2 = $yAxis - 1; $y2 <= $yAxis + 1; $y2++) {
+            if ($y2 < 0 || $y2 >= $this->opt["height"]) {
+                continue;
+            }
+            for ($x2 = $xAxis - 1; $x2 <= $xAxis + 1; $x2++) {
+                if (($x2 == $xAxis && $y2 == $yAxis) || ($x2 < 0 || $x2 >= $this->opt["width"])) {
+                    continue;
+                }
+                if ($this->cells[$y2][$x2]) {
+                    $neighbors += 1;
+                }
+            }
+        }
+        return $neighbors;
+    }
+
+    protected function clearScreen(): string
+    {
         return sprintf("\033\143");
     }
 
-    protected function generateCells() {
+    protected function generateCells(): object
+    {
 
         for ($x = 0; $x < $this->opt["width"]; $x++) {
             for ($y = 0; $y < $this->opt["height"]; $y++) {
                 $cell = 0;
                 $seed = mt_rand(0, $this->opt["odds"]);
-                if ($seed ===  $this->opt["odds"]) {
+                if ($seed === $this->opt["odds"]) {
                     $cell = 1;
                 }
                 $this->cells[$y][$x] = $cell;
@@ -107,7 +183,26 @@ condensed into the following:
         return $this;
     }
 
-    protected function render(){
+    protected function countCells(): int
+    {
+        $count = 0;
+        foreach ($this->cells as $y => $row) {
+            foreach (array_keys($row) as $x) {
+                if ($this->cells[$y][$x] === 1) {
+                    $count++;
+                }
+            }
+        }
 
+        return $count;
+    }
+
+    protected function getTime(float $seconds): string
+    {
+        $hour = floor($seconds / 3600);
+        $min = floor($seconds / 60 % 60);
+        $sec = floor($seconds % 60);
+
+        return sprintf('%02d:%02d:%02d', $hour, $min, $sec);
     }
 }
